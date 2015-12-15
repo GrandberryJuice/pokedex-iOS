643af0df4759324b77f83a6f185d8adb34bc8393
kenneth Grandberry <tbkots2@yahoo.com>
2015-12-14 21:26:43 -0600

    Initial Commit

---
+++ pokedex-by-kenny/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pokedex-by-kenny
+//
+//  Created by KENNETH GRANDBERRY on 12/14/15.
+//  Copyright © 2015 KENNETH GRANDBERRY. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
